3ee76847b9bdef7fbf4462f5ce68749f2c10fe9f
fai-sas <craftedbyfaisal@gmail.com>
2024-09-14 20:06:55 +0600

    tanstack query added in providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,11 @@
-import "@/src/styles/globals.css";
-import { Metadata, Viewport } from "next";
-import { Link } from "@nextui-org/link";
-import clsx from "clsx";
-
-import { Providers } from "./providers";
-
-import { siteConfig } from "@/src/config/site";
-import { fontSans } from "@/src/config/fonts";
-import { Navbar } from "@/src/components/UI/navbar";
+import '@/src/styles/globals.css'
+import { Metadata, Viewport } from 'next'
+import { Link } from '@nextui-org/link'
+import clsx from 'clsx'
+import { siteConfig } from '@/src/config/site'
+import { fontSans } from '@/src/config/fonts'
+import { Navbar } from '@/src/components/UI/navbar'
+import { Providers } from '../lib/Providers'
 
 export const metadata: Metadata = {
   title: {
@@ -16,35 +14,35 @@ export const metadata: Metadata = {
   },
   description: siteConfig.description,
   icons: {
-    icon: "/favicon.ico",
+    icon: '/favicon.ico',
   },
-};
+}
 
 export const viewport: Viewport = {
   themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
   ],
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html suppressHydrationWarning lang="en">
+    <html suppressHydrationWarning lang='en'>
       <head />
       <body
         className={clsx(
-          "min-h-screen bg-background font-sans antialiased",
+          'min-h-screen bg-background font-sans antialiased',
           fontSans.variable
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+        <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
           {children}
         </Providers>
       </body>
     </html>
-  );
+  )
 }
